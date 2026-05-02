2564e364eeb7956115c155968536063d76047aae
darkessence87 <darkessence87@gmail.com>
2026-05-02 14:19:28 +0200

    Update EntryPoint.cpp to new TestLib::run(CmdOptions) API

--- tests/EntryPoint.cpp
+++ tests/EntryPoint.cpp
@@ -19,20 +19,24 @@ extern void register_all_tests();
 
 int main(int argc, char *argv[])
 {
-    LOG_INFO_STATIC("Start tests main");
-
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunsafe-buffer-usage-in-container"
     auto args = std::span<char *> {argv, static_cast<size_t>(argc)};
 #pragma clang diagnostic pop
     auto opts = psi::test::TestLib::parse_args(args);
 
+    if (!opts.list_tests) {
+        LOG_INFO_STATIC("Start tests main");
+    }
+
     psi::test::TestLib::init();
     register_all_tests();
-    int result = psi::test::TestLib::run(opts.filter);
+    int result = psi::test::TestLib::run(opts);
 
     psi::test::TestLib::destroy();
-    LOG_INFO_STATIC("Exit tests main");
+    if (!opts.list_tests) {
+        LOG_INFO_STATIC("Exit tests main");
+    }
 
     return result;
 }
